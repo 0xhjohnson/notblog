a7e7f935514cef37b640ccd5482f63bbc1f5570a
0xhjohnson <0xhjohnson@protonmail.com>
2021-06-13 17:27:34 -0400

    pull author in footer from config

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import CONFIG from '@/config';
 
 export default function Footer() {
   return (
     <footer className="flex justify-between items-center py-10 border-t">
-      <p className="text-gray-500">&copy; 2021 Hunter Johnson</p>
+      <p className="text-gray-500">&copy; 2021 {CONFIG.author}</p>
     </footer>
   );
 }
